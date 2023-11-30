391e11f360e08fa03a304956473f988d285441f8
DedLad <adheshathrey2004@gmail.com>
2023-11-30 14:21:18 +0530

    added dynamic icons

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import requests
 from flask import Flask,request,render_template
 import SECRETS
+import webbrowser
+
 
 app =Flask(__name__)
 @app.route('/',methods=["POST","GET"])
@@ -8,19 +10,27 @@ def test():
     if request.method == "POST":
         code = request.form.get("code")
         key= SECRETS.x
-        r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={code}&appid={key}")
+        r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={code}&appid={key}&units=metric")
         
         class vars:
             s = r.json()['main']
+            d=r.json()['weather'][0]
             temp = s['temp']
             min = s['temp_min']
             max = s['temp_max']
             hum = s['humidity']
+            m = d['main'].upper()
+            des= d['description'].upper()
+            icon = d['icon']
         print(r.json())
         # return r.json()#,str(temp)
         return render_template('dump.html',vars=vars)#temp=temp,min=min,max=max,hum=hum)
+    
     return render_template('base.html')
+
 
 if __name__ == '__main__':
        app.run(debug='true')
+
+webbrowser.open('http://127.0.0.1:5000')
        
